cae7f30d6f1c18d8387c42bcc9b5113c62275005
Maija <maijaaaw@gmail.com>
2022-03-27 14:51:11 +0300

    Proxy requests

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,34 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
+import io from 'socket.io-client';
+import { onMounted, onUnmounted, ref } from 'vue';
 import Home from './components/Home.vue';
+
+const x = ref(0)
+const y = ref(0)
+
+const socket = ref();
+const game = ref();
+
+function update(data) {
+  game.value = data.game;
+}
+
+onMounted(() => {
+  socket.value = io();
+
+  fetch('/api')
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+  console.log('socket', socket.value);
+  socket.value.on('update', update);
+})
+
+onUnmounted(() => {
+  if (socket.value) socket.value.close()
+})
 
 </script>
 
@@ -31,7 +58,6 @@ body {
 
 }
 #app {
-  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: calc(100% - 4em);
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      // string shorthand
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/socket.io': {
+        target: 'ws://localhost:8000',
+        ws: true
+      }
+    }
+  }
 })

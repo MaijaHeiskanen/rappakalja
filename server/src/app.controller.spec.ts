925e28e4ac7cf14e097c00dcf89a4c0a66345314
Maija <maijaaaw@gmail.com>
2022-05-15 22:56:32 +0300

    Add tests

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -1,22 +1,51 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocketGateway } from './socket.gateway';
+import * as request from 'supertest';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, SocketGateway],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('Should return "Hello World!"', () => {
+      return request(app.getHttpServer()).get('/').expect(200);
+    });
+  });
+
+  describe('create', () => {
+    it('Should return new game object', () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const module = require('./utils/generateNumberString');
+
+      jest
+        .spyOn(module, 'generateNumberString')
+        .mockImplementation(() => '12345');
+
+      return request(app.getHttpServer())
+        .post('/create')
+        .send({ socketId: 'socketId' })
+        .expect(201, {
+          room: '12345',
+          players: [{ socketId: 'socketId', name: '', state: 0 }],
+          gameState: 0,
+          word: '',
+          definitions: [],
+          allDefinitions: [],
+          correctDefinitions: [],
+          points: [],
+        });
     });
   });
 });
